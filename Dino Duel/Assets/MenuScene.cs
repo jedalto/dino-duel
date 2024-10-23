d7f62aff44ef106a5751879403f910c451b261ad
Damian Sclafani <damians@email.sc.edu>
2024-10-23 19:46:30 -0400

    menuUpdate

--- Dino Duel/Assets/MenuScene.cs
+++ Dino Duel/Assets/MenuScene.cs
@@ -18,7 +18,7 @@ public class MenuScene : MonoBehaviour
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        background = GameObject.Find("GameBackground");
+        background = GameObject.Find("background");
         title = GameObject.Find("Title");
         press = GameObject.Find("Press");
         p1 = GameObject.Find("P1Controls");
@@ -29,8 +29,8 @@ public class MenuScene : MonoBehaviour
             //.Append(background.GetComponent<SpriteRenderer>().DOFade(0, 1))
             .Append(title.GetComponent<Text>().DOText("Dino Duel", 1))
             .Insert(1, press.GetComponent<Text>().DOText("Press Space to Play", 3))
-            .Insert(1, p1.GetComponent<Text>().DOText("Player 1 Uses WASD \nw = jump \n a = left \ns = shoot \nd = right", 2))
-            .Insert(1, p2.GetComponent<Text>().DOText("Player 2 Uses Arrow Keys \nUp = jump \nLeft = left \nDown = shoot \nRight = right", 2));
+            .Insert(1, p1.GetComponent<Text>().DOText("Player 1 Controls \nw = jump \na = left \ns = shoot \nd = right", 2))
+            .Insert(1, p2.GetComponent<Text>().DOText("Player 2 Controls \nUp = jump \nLeft = left \nDown = shoot \nRight = right", 2));
     }
 
     // Update is called once per frame
